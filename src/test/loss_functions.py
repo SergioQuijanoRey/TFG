ae2d79eb2ceb51e1d5ed28675ca129ede5fcdf90
Sergio <sergiquijano@gmail.com>
2022-02-26 20:26:35 +0100

    test #6: added some basic examples of distances to test

--- src/test/loss_functions.py
+++ src/test/loss_functions.py
@@ -3,10 +3,10 @@ Testing of loss functions
 """
 
 import unittest
-
-import numpy as np
 import torch
-from src.lib.loss_functions import *
+import math
+
+from src.lib.loss_functions import distance_function
 
 # Number of repetitions, when needed
 NUMBER_OF_REPETITIONS = 100
@@ -40,3 +40,28 @@ class TestBasicLossFunction(unittest.TestCase):
             # Assert distance is greater than zero
             self.assertGreater(distance, 0.0)
 
+    def test_some_known_examples(self):
+
+        # First basic example
+        first = torch.tensor([0.0, 0.0])
+        second = torch.tensor([1.0, 1.0])
+
+        dist_computed = distance_function(first, second)
+        dist_expect = math.sqrt(2.0)
+        self.assertAlmostEquals(dist_computed, dist_expect)
+
+        # Second basic example
+        first = torch.tensor([1.0, 1.0])
+        second = torch.tensor([2.0, 2.0])
+
+        dist_computed = distance_function(first, second)
+        dist_expect = math.sqrt(2.0)
+        self.assertAlmostEquals(dist_computed, dist_expect)
+
+        # Third basic example
+        first = torch.tensor([1.0, 1.0])
+        second = torch.tensor([-4.0, 2.0])
+
+        dist_computed = distance_function(first, second)
+        dist_expect = math.sqrt(26)
+        self.assertAlmostEquals(dist_computed, dist_expect)
