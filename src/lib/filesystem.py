a685107cf9f528ece19ec10dbd261b1afbf29f58
Sergio Quijano <sergiquijano@gmail.com>
2022-09-19 12:53:40 +0200

    docs: improved docs for the file system loading method

--- src/lib/filesystem.py
+++ src/lib/filesystem.py
@@ -5,6 +5,8 @@ import pathlib
 import torch
 from torch import nn
 
+from typing import Callable
+
 # TODO -- I have seen that along the net we can save some state in form of a python dict
 #      -- Source: https://www.programcreek.com/python/?code=drimpossible%2FDeep-Expander-Networks%2FDeep-Expander-Networks-master%2Fcode%2Fmodels%2F__init__.py
 def save_model(net: nn.Module, folder_path: str, file_name: str) -> None:
@@ -22,17 +24,15 @@ def save_model(net: nn.Module, folder_path: str, file_name: str) -> None:
     save_path = os.path.join(folder_path, file_name)
     torch.save(net.state_dict(), save_path)
 
-# TODO -- change model_class to a lambda that initializes a net instance
-#   This way, we can initialize classes with parameters (such building block of ResNet)
-def load_model(model_path: str, model_class: nn.Module) -> nn.Module:
+def load_model(model_path: str, model_class: Callable[nn.Module]) -> nn.Module:
     """
     Loads a model from disk and returns it
 
     Parameters:
     ===========
     model_path: the path to the disk file
-    model_class: the class corresponding to the model in disk
-                 because we need to create an object of some given type
+    model_class: lambda function of the constructor of the net class
+                 this way, we instance a new object of that class, and load the state from disk
 
     Returns:
     ========
