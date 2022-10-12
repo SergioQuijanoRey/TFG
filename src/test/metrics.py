420893ca555da7c82dfd9d473e1f3389534109e7
Sergio Quijano <sergiquijano@gmail.com>
2022-10-12 13:40:15 +0200

    test #33: added some passing tests

--- src/test/metrics.py
+++ src/test/metrics.py
@@ -33,3 +33,51 @@ class TestComputeIntraclusterDistances(unittest.TestCase):
         self.assertAlmostEqual(intra_cluster_distances[1][0], 2.23606797749979)
         self.assertAlmostEqual(intra_cluster_distances[1][1], 9.0)
         self.assertAlmostEqual(intra_cluster_distances[1][2], 7.0710678118654755)
+
+    def test_clusters_with_one_element_produce_no_distance(self):
+        targets = [0, 1, 2, 3, 4, 5]
+        images = torch.Tensor([
+            # Class 0 images
+            [0, 0, 0],
+            [0, 0, 1],
+            [0, 0, 2],
+
+            # Class 1 images
+            [1, 0, 0],
+            [3, 1, 0],
+            [10, 0, 0],
+        ])
+
+        # Compute the intracluster distances
+        dict_of_classes = utils.precompute_dict_of_classes(targets)
+        intra_cluster_distances = metrics.compute_intracluster_distances(dict_of_classes, images)
+
+        # Distances corresponding to the first cluster
+        self.assertEqual(len(intra_cluster_distances), 0)
+
+
+    def test_clusters_with_two_elements_produce_distance(self):
+        targets = [0, 0, 1, 1, 2, 2]
+        images = torch.Tensor([
+            # Class 0 images
+            [0, 0, 0],
+            [0, 0, 1],
+
+            # Class 1 images
+            [0, 0, 2],
+            [1, 0, 0],
+
+            # Class 2 images
+            [3, 1, 0],
+            [10, 0, 0],
+        ])
+
+        # Compute the intracluster distances
+        dict_of_classes = utils.precompute_dict_of_classes(targets)
+        intra_cluster_distances = metrics.compute_intracluster_distances(dict_of_classes, images)
+
+        # Some checks
+        self.assertEqual(len(intra_cluster_distances), 3)
+        self.assertAlmostEqual(intra_cluster_distances[0][0], 1.0)
+        self.assertAlmostEqual(intra_cluster_distances[1][0], 2.23606797749979)
+        self.assertAlmostEqual(intra_cluster_distances[2][0], 7.0710678118654755)
