e2db851a605762ee175dd650e55386ae32b0e5fe
Sergio Quijano <sergiquijano@gmail.com>
2022-10-12 20:07:47 +0200

    test #34: add failing test for new intercluster functionality

--- src/test/metrics.py
+++ src/test/metrics.py
@@ -116,3 +116,42 @@ class TestComputeClusterSizesMetrics(unittest.TestCase):
         self.assertAlmostEqual(cluster_metrics["mean"], 5.5)
         self.assertAlmostEqual(cluster_metrics["sd"], 3.5)
 
+class TestComputeInterclusterDistances(unittest.TestCase):
+
+    def test_basic_case(self):
+
+        # Basic data for the test
+        dict_of_classes = {
+            0: [0, 1],
+            1: [2, 3],
+            2: [4, 5]
+        }
+        distances = {
+            (0, 1): 1,
+            (0, 2): 2,
+            (0, 3): 3,
+            (0, 4): 1.5,
+            (0, 5): 3.4,
+
+            (1, 2): 6,
+            (1, 3): 1.1,
+            (1, 4): 0.5,
+            (1, 5): 1.1111,
+
+            (2, 3): 2.222,
+            (2, 4): 3.333,
+            (2, 5): 4.444,
+
+            (3, 4): 5.555,
+            (3, 5): 0.102,
+
+            (4, 5): 0.01,
+        }
+
+        # Compute the intercluster distances
+        intercluster_distances = metrics.compute_intercluster_distances(distances, dict_of_classes)
+
+        # Make some checks on the returned data
+        self.assertAlmostEqual(intercluster_distances[0, 1], 1.1)
+        self.assertAlmostEqual(intercluster_distances[0, 2], 0.5)
+        self.assertAlmostEqual(intercluster_distances[1, 2], 0.102)
