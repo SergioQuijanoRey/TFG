319433e217ffbc615a6a8dfbdf26f4c793892f73
Sergio Quijano <sergiquijano@gmail.com>
2022-12-21 14:37:44 +0100

    docs, build, fix: pinning shell.nix because last version of texlive enviroment is broken

--- thesis/shell.nix
+++ thesis/shell.nix
@@ -1,6 +1,10 @@
-{ pkgs ? import <nixpkgs> { } }:
-with pkgs;
 let
+    # Pinning, because last version of the texlive enviroment is broken
+    pkgs = import (builtins.fetchTarball {
+        name = "NixOS_Stable_05_September_2022";
+        url = "https://github.com/nixos/nixpkgs/archive/7d7622909a38a46415dd146ec046fdc0f3309f44.tar.gz";
+        sha256 = "016dcmy9gkmrzd5gsmvz4rd6gh5phf2kg7jf8g2y276v5km50nwf";
+    }) {};
 
     # Declare the latex enviroment (base enviroment plus additional packages)
     custom_tex_env = (pkgs.texlive.combine {
@@ -24,8 +28,8 @@ let
         ;
     });
 in
-mkShell {
-    buildInputs = [
+pkgs.mkShell {
+    buildInputs = with pkgs; [
         # TODO -- remove this dependency
         zsh
 
