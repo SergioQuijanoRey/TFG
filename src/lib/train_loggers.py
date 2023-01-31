870ac97c9931d605ca53b72c7d71ca3ee24661dd
Sergio Quijano <sergiquijano@gmail.com>
2023-01-31 19:09:37 +0100

    refactor: deleted code to use tensorboard
    - We're using wandb, so there is no need for tensorboard code

--- src/lib/train_loggers.py
+++ src/lib/train_loggers.py
@@ -10,7 +10,6 @@ import logging
 file_logger = logging.getLogger("MAIN_LOGGER")
 
 import src.lib.core as core
-import src.lib.board as board
 import src.lib.metrics as metrics
 
 
@@ -134,109 +133,6 @@ class CompoundLogger(TrainLogger):
 
         # Now, we can tell if there's at least one logger that wants to log
         return any(self.logger_should_log)
-
-# TODO -- remove this class because we're not using it
-class ClassificationLogger(TrainLogger):
-    """
-    Logger for a classifaction problem
-    """
-
-    def __init__(self, net: nn.Module, iterations, loss_func, training_perc: float = 1.0, validation_perc: float = 1.0):
-        """
-        Initializes the logger
-
-        Parameters:
-        ===========
-        net: the net we are testing
-        iterations: how many iterations we have to wait to log again
-        loss_func: the loss func we are using to train
-        training_perc: the percentage of the training set we are going to use to compute metrics
-        validation_perc: the percentage of the validation set we are going to use to compute metrics
-        tensorboardwriter: writer to tensorboard logs
-        """
-        self.iterations = iterations
-        self.loss_func = loss_func
-        self.net = net
-        self.training_perc = training_perc
-        self.validation_perc = validation_perc
-
-        # Tensorboard named with the date/time stamp
-        self.name = core.get_datetime_str()
-        self.tensorboardwriter = board.get_writer(name = self.name)
-
-    def log_process(
-        self,
-        train_loader: DataLoader,
-        validation_loader: DataLoader,
-        epoch: int,
-        epoch_iteration: int
-    ) -> Dict[str, float]:
-
-        # For more performance
-        with torch.no_grad():
-
-            # Even more performance
-            self.net.eval()
-
-            # Trainning loss and accuracy
-            max_examples = int(len(train_loader.dataset) * self.training_perc)
-            mean_train_loss = metrics.calculate_mean_loss(self.net, train_loader, max_examples, self.loss_func)
-            mean_train_acc = metrics.calculate_accuracy(self.net, train_loader, max_examples)
-
-            # Validation loss and accuracy
-            max_examples = int(len(validation_loader.dataset) * self.training_perc)
-            mean_val_loss = metrics.calculate_mean_loss(self.net, validation_loader, max_examples, self.loss_func)
-            mean_val_acc = metrics.calculate_accuracy(self.net, validation_loader, max_examples)
-
-
-
-        # Set again net to training mode
-        self.net.train()
-
-        # Output to the user
-        # We don't care about epochs starting in 0, but with iterations is weird
-        # ie. epoch 0 it 199 instead of epoch 0 it 200
-        print(f"[{epoch} / {epoch_iteration}] <-- ")
-        print(f"Training loss: {mean_train_loss}")
-        print(f"Validation loss: {mean_val_loss}")
-        print(f"Training acc: {mean_train_acc}")
-        print(f"Validation acc: {mean_val_acc}")
-        print("")
-
-        # Sending this metrics to tensorboard
-        curr_it = epoch_iteration * train_loader.batch_size + epoch * len(train_loader.dataset) # The current iteration taking in count
-                                                                # that we reset iterations at the end
-                                                                # of each epoch
-
-        # Send data to tensorboard
-        # We use side-by-side training / validation graphics
-        self.tensorboardwriter.add_scalars(
-            "Loss",
-            {
-                "Training loss": mean_train_loss,
-                "Validation loss": mean_val_loss,
-            },
-            curr_it
-        )
-        self.tensorboardwriter.add_scalars(# Have train / val acc in same graph to compare
-            "Accuracy",
-            {
-                "Training acc": mean_train_acc,
-                "Validation acc": mean_val_acc,
-            },
-            curr_it
-        )
-        self.tensorboardwriter.flush() # Make sure that writer writes to disk
-
-        # TODO -- this return is wrong, return the actual metrics
-        return dict()
-
-
-    def should_log(self, iteration: int) -> bool:
-        if iteration % self.iterations == 0:
-            return True
-
-        return False
 
 # TODO -- check if I can use same logger for offline and online triplet training
 class TripletLoggerOffline(TrainLogger):
