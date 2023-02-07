8ecc648f11425ae6bf8f89457f32aa46cdfaf691
Sergio Quijano <sergiquijano@gmail.com>
2023-02-07 19:07:05 +0100

    fix: now using proper parameters in `compute_cluster_sizes_metrics`

--- src/lib/train_loggers.py
+++ src/lib/train_loggers.py
@@ -332,6 +332,11 @@ class IntraClusterLogger(TrainLogger):
         self.train_percentage = train_percentage
         self.validation_percentage = validation_percentage
 
+        # Choose wether or not use fast implementation for underlying functions
+        # that rely on `__get_portion_of_dataset_and_embed`
+        # Underlying functions is about computing cluster metrics
+        self.fast_implementation = False
+
     def log_process(
         self,
         train_loader: DataLoader,
@@ -354,8 +359,18 @@ class IntraClusterLogger(TrainLogger):
             self.net.eval()
 
             # Get the two metrics
-            train_metrics = metrics.compute_cluster_sizes_metrics(train_loader, self.net, train_max_examples)
-            validation_metrics = metrics.compute_cluster_sizes_metrics(validation_loader, self.net, validation_max_examples)
+            train_metrics = metrics.compute_cluster_sizes_metrics(
+                train_loader,
+                self.net,
+                train_max_examples,
+                self.fast_implementation
+            )
+            validation_metrics = metrics.compute_cluster_sizes_metrics(
+                validation_loader,
+                self.net,
+                validation_max_examples,
+                self.fast_implementation,
+            )
 
 
         # Get the network in training mode again
