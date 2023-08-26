93b030a4453cfa05cdb11e1d9b087eb92d416f82
Sergio Quijano <sergiquijano@gmail.com>
2023-08-26 14:15:28 +0200

    feat: added an sql query to view all the hp tuning runs

--- optuna_queries.sql
+++ optuna_queries.sql
@@ -1,5 +1,10 @@
 -- SQL queries to execute on SQLITE3 database that optuna uses to store information
 
+-- Sometimes hp tuning almost cannot produce combinations of parameters that lead
+-- to succesful trainings. So it is useful to query for all succesful trainings,
+-- so we can detect some problems
+SELECT * FROM trial_values;
+
 -- Get the info of the best trial value (trial value is the float that we are
 -- optimizing)
 SELECT *  FROM trial_values WHERE value == (SELECT MAX(value) FROM trial_values);
